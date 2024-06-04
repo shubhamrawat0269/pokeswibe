f4cb178e43ca8b5c3feb0793f8dd1a768de365c4
shubham Rawat <rawatshubam269@gmail.com>
2024-06-04 19:11:37 +0530

    Loading Added

--- src/components/pokemonCards/PokemonCards.jsx
+++ src/components/pokemonCards/PokemonCards.jsx
@@ -66,66 +66,70 @@ const PokemonCards = () => {
 
   return (
     <div className="w-inherit h-inherit grid place-items-center">
-      {pokeData.map((character, index) => (
-        <TinderCard
-          ref={childRefs[index]}
-          className="absolute"
-          key={character?.data?.name}
-          onSwipe={(dir) => swiped(dir, character?.data?.name, index)}
-          onCardLeftScreen={() => outOfFrame(character?.data?.name, index)}
-        >
-          <div className=" bg-slate-200 p-2 dark:bg-neutral-900 rounded-2xl shadow-xl bg-cover bg-center">
-            <figure className="flex flex-col gap-2 justify-center items-center relative">
-              <img
-                className="w-[20rem] aspect-square"
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${character?.data?.id}.svg`}
-                alt={character?.data?.name}
-              />
-              <figcaption className="text-4xl dark:text-white">
-                {(character?.data?.name).toUpperCase()}
-              </figcaption>
-              <div>
+      {pokeData?.length > 0 ? (
+        pokeData.map((character, index) => (
+          <TinderCard
+            ref={childRefs[index]}
+            className="absolute"
+            key={character?.data?.name}
+            onSwipe={(dir) => swiped(dir, character?.data?.name, index)}
+            onCardLeftScreen={() => outOfFrame(character?.data?.name, index)}
+          >
+            <div className=" bg-slate-200 p-2 dark:bg-neutral-900 rounded-2xl shadow-xl bg-cover bg-center">
+              <figure className="flex flex-col gap-2 justify-center items-center relative">
                 <img
-                  className="w-[4rem] absolute top-1 right-1"
-                  src="like.png"
-                  alt="likeImg"
+                  className="w-[20rem] aspect-square"
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${character?.data?.id}.svg`}
+                  alt={character?.data?.name}
                 />
+                <figcaption className="text-4xl dark:text-white">
+                  {(character?.data?.name).toUpperCase()}
+                </figcaption>
+                <div>
+                  <img
+                    className="w-[4rem] absolute top-1 right-1"
+                    src="like.png"
+                    alt="likeImg"
+                  />
+                </div>
+              </figure>
+              <div className="flex justify-around items-center gap-1 my-5">
+                {character?.data?.abilities.map((poke, id) => {
+                  return (
+                    <div
+                      className="py-1 px-4 rounded-lg text-white"
+                      key={id}
+                      style={{
+                        background: `${optionsColor[useGenerateRandomId()]}`,
+                      }}
+                    >
+                      {poke?.ability?.name}
+                    </div>
+                  );
+                })}
               </div>
-            </figure>
-            <div className="flex justify-around items-center gap-1 my-5">
-              {character?.data?.abilities.map((poke, id) => {
-                return (
-                  <div
-                    className="py-1 px-4 rounded-lg text-white"
-                    key={id}
-                    style={{
-                      background: `${optionsColor[useGenerateRandomId()]}`,
-                    }}
-                  >
-                    {poke?.ability?.name}
-                  </div>
-                );
-              })}
+              <div className="flex flex-wrap justify-between">
+                <button
+                  className="p-2.5 bg-teal-700 rounded-md border-none text-white hover:scale-105 transition-all m-2.5 font-bold w-36"
+                  style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+                  onClick={() => swipe("left")}
+                >
+                  Dislike
+                </button>
+                <button
+                  className="p-2.5 rounded-md bg-cyan-900 border-none text-white hover:scale-105  transition-all m-2.5 font-bold w-36"
+                  style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+                  onClick={() => swipe("right", character?.data)}
+                >
+                  Like
+                </button>
+              </div>
             </div>
-            <div className="flex flex-wrap justify-between">
-              <button
-                className="p-2.5 bg-teal-700 rounded-md border-none text-white hover:scale-105 transition-all m-2.5 font-bold w-36"
-                style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
-                onClick={() => swipe("left")}
-              >
-                Dislike
-              </button>
-              <button
-                className="p-2.5 rounded-md bg-cyan-900 border-none text-white hover:scale-105  transition-all m-2.5 font-bold w-36"
-                style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
-                onClick={() => swipe("right", character?.data)}
-              >
-                Like
-              </button>
-            </div>
-          </div>
-        </TinderCard>
-      ))}
+          </TinderCard>
+        ))
+      ) : (
+        <h1 className="text-3xl">Loading...</h1>
+      )}
     </div>
   );
 };
